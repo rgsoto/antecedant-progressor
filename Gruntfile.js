ebde7575a96467d5f2cbc91a7a5bf34999e01fde
usufruct <usufruct@gmail.com>
2014-10-05 17:59:53 -0500

    specific grunt tasks to start and stop postgres pg is defined locally, in ./postgres_local as operating on port 8888

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
 'use strict';
+var spawn = require('child_process').spawn;
+var fs = require('fs');
 
 module.exports = function(grunt) {
   // Show elapsed time at the end
@@ -47,6 +49,48 @@ module.exports = function(grunt) {
         tasks: ['jshint:test', 'mochacli']
       }
     }
+  });
+
+  // PG server for tests
+  grunt.registerTask('startTestDB', function() {
+    var done = this.async();
+
+    var out = fs.openSync('./local_postgres/out.log', 'a');
+    var err = fs.openSync('./local_postgres/out.log', 'a');
+
+    // check for .pid file before starting server
+    if (fs.existsSync('./local_postgres/postmaster.pid')) {
+      var postmaster = fs.readFileSync('./local_postgres/postmaster.pid', 'utf8');
+      console.log("postgres already running");
+      console.log("kill it with the first line (kill <pid>) of what follows");
+      console.log(postmaster);
+      done();
+    } else {
+      var pg = spawn('postgres', ['-D', 'local_postgres'],
+        { 'stdio': ['ignore',out,err],
+          'detached': true });
+      pg.unref();
+      console.log(pg.pid);
+      done();
+    }
+  });
+  grunt.registerTask('stopTestDB', function() {
+    var done = this.async();
+
+    // check for .pid file before stopping server
+    if (fs.existsSync('./local_postgres/postmaster.pid')) {
+      var postmaster = fs.readFileSync('./local_postgres/postmaster.pid', 'utf8');
+      var pid = postmaster.match(/^.*$/m)[0];
+      console.log("killing task id: " + pid);
+      spawn('kill', [pid]);
+      done();
+    } else {
+      console.log("postgres is not running");
+      done();
+    }
+  });
+  grunt.registerTask('pingPostgres', function() {
+    var pgReady = spawn('pg_isready', ['--port=8888'], { 'stdio': [0,1,2]});
   });
 
   // Default task.
--- test/antecedant-progressor_test.js
+++ test/antecedant-progressor_test.js
@@ -1,7 +1,11 @@
 /*global describe,it*/
 'use strict';
-var assert = require('assert'),
-  antecedantProgressor = require('../lib/antecedant-progressor.js');
+var assert = require('assert');
+var antecedantProgressor = require('../lib/antecedant-progressor.js');
+
+var client = antecedantProgressor.connect();
+console.log(client); // async,, does not print anything
+
 
 describe('antecedant-progressor node module.', function() {
   it('must be awesome', function() {
